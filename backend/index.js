eb279451b38c255f80e5a4d3858167460ed37513
AGNIK DASTIDAR <147322194+agnik04dastidar@users.noreply.github.com>
2025-12-12 10:55:03 +0530

    Update index.js
    for render deployment

--- backend/index.js
+++ backend/index.js
@@ -331,10 +331,10 @@ const port = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
 
 server.listen(port, () => {
